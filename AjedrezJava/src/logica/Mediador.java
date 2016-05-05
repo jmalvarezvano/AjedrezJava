fc0517bdb3cf50d3ebceb5e1c13daab8168bdcaf
Dani <Dani@192.168.0.165>
2016-05-05 22:17:03 +0200

    mediador

--- AjedrezJava/src/logica/Mediador.java
+++ AjedrezJava/src/logica/Mediador.java
@@ -34,14 +34,38 @@ public class Mediador {
 	}
 
 	public boolean moverPieza(int origenX, int origenY, int destinoX, int destinoY, Jugador j) {
-		Celda origen = tablero.getCelda(origenX, origenY);
-		Celda destino = tablero.getCelda(destinoX, destinoY);
-		
+		Celda origen;
+		Celda destino;
+		try{
+		origen = tablero.getCelda(origenX, origenY);		
+		destino = tablero.getCelda(destinoX, destinoY);
+		} catch(Exception e) {return false;}		
 		if (origen.getPieza() == null || !origen.getPieza().getJugador().equals(j)) return false;
-		
+		if (destino.getPieza().getJugador().equals(j)) return false;
+		if (movimientoValido(origenX, origenY, destinoX, destinoY, origen.getPieza().getMovimiento()) {
+				destino.setPieza(origen.getPieza());
+				return true;
+		}		
 		return false;
 	}
-
+	
+	public boolean movimientoValido(int origenX, int origenY, int destinoX, int destinoY, String movimiento) {
+		int xRelativo = destinoX - origenX;
+		int yRelativo = destinoY - origenY;
+		switch (movimiento) {
+		case "alfil": 
+			if(Math.abs(xRelativo) == Math.abs(yRelativo)){
+				for(int x = origenX + 1, y = origenY + 1; x < destinoX && y < destinoY; x++, y++) {
+					if(tablero.getCelda(x, y).getPieza() != null) return false;
+				}
+				return true;
+			}
+			break;
+		case "caballo":
+			if(Math.abs(xRelativo) 
+		}
+		return false;
+	}
 	
 	
 }
